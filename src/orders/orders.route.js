4e79ba4d3d16d0dd5df6c6a093fe26bea565c249
AjayMurmu007 <ajaymurmu007@gmail.com>
2025-08-27 23:09:16 +0530

    url update

--- src/orders/orders.route.js
+++ src/orders/orders.route.js
@@ -26,9 +26,12 @@ router.post("/create-checkout-session", async (req, res) => {
             payment_method_types: ["card"],
             line_items: lineItems,
             mode: "payment",
-            success_url:
-                `http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: `http://localhost:5173/cancel`,
+            // success_url:
+            //     `http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}`,
+            // cancel_url: `http://localhost:5173/cancel`,
+             success_url:
+                `https://lebaba-full-ecomerce-frontend.vercel.app/success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `https://lebaba-full-ecomerce-frontend.vercel.app/cancel`,
         });
 
         res.json({ id: session.id });
